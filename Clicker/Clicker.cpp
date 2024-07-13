69b08107032c806688683be3b19420163a717618
matcher9131 <matcher9131cd@gmail.com>
2024-07-13 11:12:49 +0900

    Implement auto click

--- Clicker/Clicker.cpp
+++ Clicker/Clicker.cpp
@@ -1,10 +1,15 @@
 ﻿// Clicker.cpp : アプリケーションのエントリ ポイントを定義します。
 //
 
+#include <chrono>
+#include <thread>
 #include "framework.h"
 #include "Clicker.h"
 
 #define MAX_LOADSTRING 100
+#define MOUSE_LOCATION_X(x) ((ULONG)x * 0xFFFF / GetSystemMetrics(SM_CXSCREEN))
+#define MOUSE_LOCATION_Y(y) ((ULONG)y * 0xFFFF / GetSystemMetrics(SM_CYSCREEN))
+constexpr int TIMER_INTERVAL = 100;
 
 // グローバル変数:
 HINSTANCE hInst;                                // 現在のインターフェイス
@@ -14,6 +19,8 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ ク
 HWND activeWindow = NULL;
 WCHAR activeWindowClass[MAX_LOADSTRING];
 Settings settings;
+bool isWorking = false;
+int remainTime = 10000;
 
 // このコード モジュールに含まれる関数の宣言を転送します:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -32,6 +39,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // TODO: ここにコードを挿入してください。
     // 設定ファイルの読み込み
     settings = LoadSettings();
+    remainTime = settings.interval;
     //
 
     // グローバル文字列を初期化する
@@ -132,6 +140,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+    case WM_CREATE:
+        SetTimer(hWnd, 1, TIMER_INTERVAL, NULL);
+        break;
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -154,13 +165,56 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: HDC を使用する描画コードをここに追加してください...
-
+            TextOutW(hdc, 10, 10, isWorking ? L"Working" : L"Pending", 7);
             // 
             EndPaint(hWnd, &ps);
         }
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
+        break;
+    case WM_TIMER:
+        if (wParam == 1)
+        {
+            // アクティブウィンドウを取得してクラス名がSettings.classNameと等しいかどうかを判断する
+            activeWindow = GetForegroundWindow();
+            GetClassNameW(activeWindow, activeWindowClass, MAX_LOADSTRING);
+            isWorking = lstrcmpW(activeWindowClass, settings.className.c_str()) == 0;
+            // WorkingならばremainTimeを減らす
+            if (isWorking)
+            {
+                remainTime -= TIMER_INTERVAL;
+                // remainTimeがなくなったらクリック動作をする
+                if (remainTime <= 0)
+                {
+                    RECT rect;
+                    GetWindowRect(activeWindow, &rect);
+                    long x = (rect.left + rect.right) / 2;
+                    long y = (rect.top + rect.bottom) / 2;
+                    INPUT input1 = {
+                        INPUT_MOUSE, 
+                        MOUSE_LOCATION_X(x), 
+                        MOUSE_LOCATION_Y(y),
+                        0,
+                        MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE | MOUSEEVENTF_LEFTDOWN 
+                    };
+                    SendInput(1, &input1, sizeof(INPUT));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(35));
+                    INPUT input2 = {
+                        INPUT_MOUSE,
+                        0,
+                        0,
+                        0,
+                        MOUSEEVENTF_LEFTUP
+                    };
+                    SendInput(1, &input2, sizeof(INPUT));
+                    // remainTimeをリセット
+                    remainTime = settings.interval;
+                }
+            }
+            // 再描画
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
         break;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
