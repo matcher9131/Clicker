83b56afad5202d6f2f6169f5af4f58d8a9e787d1
matcher9131 <matcher9131cd@gmail.com>
2024-07-18 08:43:56 +0900

    Make this app dialog based

--- Clicker/Resource.h
+++ Clicker/Resource.h
@@ -1,30 +1,29 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ で生成されたインクルード ファイルです。
-// 次で使用: Clicker.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_CLICKER_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_CLICKER			107
-#define IDI_SMALL				108
-#define IDC_CLICKER			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// 新しいオブジェクトの次の既定値
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// Clicker.rc で使用
 //
+#define IDC_MYICON                      2
+#define IDD_CLICKER_DIALOG              102
+#define IDS_APP_TITLE                   103
+#define IDM_EXIT                        105
+#define IDI_CLICKER                     107
+#define IDI_SMALL                       108
+#define IDC_CLICKER                     109
+#define IDR_MAINFRAME                   128
+#define IDD_MAIN_DIALOG                 129
+#define IDC_CHECK1                      1001
+#define IDC_ENABLED_CHECK               1001
+#define IDC_STATIC                      -1
+#define IDC_WORKING_LABEL               -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
